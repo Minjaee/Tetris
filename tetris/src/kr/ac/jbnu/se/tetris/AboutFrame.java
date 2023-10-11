2ae9a47af66e051a90c0d2ca48d2566b4d258f46
Minjaee <chelsea@jbnu.ac.kr>
2023-10-11 23:27:38 +0900

    메인메뉴 수정

---
+++ tetris/src/kr/ac/jbnu/se/tetris/AboutFrame.java
@@ -0,0 +1,65 @@
+package kr.ac.jbnu.se.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class AboutFrame extends JFrame implements ActionListener {
+    JButton backButton;
+    MainMenu mainMenu;
+
+    AboutFrame(){
+        // frame setup
+        this.setTitle("tetris"); // app title
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(900, 700);// window dimensions
+        this.getContentPane().setBackground(Color.black);
+
+        // logo setup
+        ImageIcon logo = new ImageIcon("images/logo.png");
+        this.setIconImage(logo.getImage());
+
+        //text declaration
+        JLabel text = new JLabel();
+        text.setText(
+                "<html>This game was developed by:<br>" +
+                "김민재<br>" +
+                "황주원<br>" +
+                        "and<br> " +
+                "성재현");
+
+
+        //text parameters setup
+        text.setForeground(Color.yellow);
+        text.setFont(new Font ("MV Boli", Font.PLAIN, 40));
+        text.setBounds(0, 0, 900, 500);
+        text.setHorizontalTextPosition(JLabel.CENTER);
+        text.setHorizontalAlignment(JLabel.CENTER);
+
+        //backButton setup
+        backButton = new JButton("Back to Main-Menu");
+        backButton.setFocusable(false);
+        backButton.setBounds(370, 580, 150, 50);
+        backButton.addActionListener(this);
+
+
+        //adding elements to the frame
+        this.setLayout(null);
+        this.add(text);
+        this.add(backButton);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //go back to main-menu if backButton pressed
+        if(e.getSource() == backButton){
+            mainMenu = new MainMenu();
+            this.setVisible(false);
+            mainMenu.setVisible(true);
+        }
+    }
+}
---
+++ tetris/src/kr/ac/jbnu/se/tetris/MainMenu.java
@@ -0,0 +1,132 @@
+package kr.ac.jbnu.se.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends JFrame implements ActionListener {
+
+    AboutFrame aboutFrame;
+
+    JPanel buttonsPanel, aboutPanel, settingsPanel;
+    JButton singlePlayerButton;
+    JButton multiPlayerButton;
+    JButton scoreboardButton;
+    JButton aboutButton, settingsButton;
+
+    public MainMenu() {
+        this.setTitle("Tetris");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(900, 700);
+
+        //logo setup
+        ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("images/logo.png"));
+        this.setIconImage(logo.getImage());
+
+        // background image setup
+        ImageIcon image = new ImageIcon(getClass().getClassLoader().getResource("images/background.png"));
+
+        // settings image setup
+        ImageIcon settings = new ImageIcon(getClass().getClassLoader().getResource("images/settings.png"));
+
+        // about image setup
+        ImageIcon about = new ImageIcon(getClass().getClassLoader().getResource("images/about.png"));
+
+        JLabel background_image = new JLabel(image);
+        background_image.setBounds(0, 0, 900, 700);
+
+        // set panel for single-, multiplayer and scoreboard buttons
+        buttonsPanel = new JPanel();
+        buttonsPanel.setBackground(Color.black);
+        buttonsPanel.setLayout(new GridLayout(3, 1));
+        buttonsPanel.setBounds(325, 480, 250, 150);
+
+        //set another panel for the about button
+        aboutPanel = new JPanel();
+        aboutPanel.setBackground(Color.black);
+        aboutPanel.setLayout(new GridLayout(1, 1));
+        aboutPanel.setBounds(70, 550, 50, 50);
+
+        //set another panel for the settings button
+        settingsPanel = new JPanel();
+        settingsPanel.setBackground(Color.black);
+        settingsPanel.setLayout(new GridLayout(1, 1));
+        settingsPanel.setBounds(780, 550, 50, 50);
+
+        //main menu header setup
+        /*
+        JLabel header = new JLabel("Tetris");
+        header.setBounds(310, 100, 900, 100);
+        header.setForeground(Color.yellow);
+        header.setFont(new Font("MV Boli", Font.PLAIN, 80));
+        */
+
+        //buttons
+        singlePlayerButton = new JButton("Single-Player");
+        singlePlayerButton.setFont(new Font("MV Boli", Font.BOLD, 21));
+        multiPlayerButton = new JButton("Multi-Player");
+        multiPlayerButton.setFont(new Font("MV Boli", Font.BOLD, 21));
+        scoreboardButton = new JButton("Score List");
+        scoreboardButton.setFont(new Font("MV Boli", Font.BOLD, 21));
+        aboutButton = new JButton(about);
+        settingsButton = new JButton(settings);
+
+        //button action setup
+        singlePlayerButton.addActionListener(this);
+        singlePlayerButton.setFocusable(false);
+
+        multiPlayerButton.addActionListener(this);
+        multiPlayerButton.setFocusable(false);
+
+        scoreboardButton.addActionListener(this);
+        scoreboardButton.setFocusable(false);
+
+        buttonsPanel.add(singlePlayerButton);
+        buttonsPanel.add(multiPlayerButton);
+        buttonsPanel.add(scoreboardButton);
+
+        // about button setup
+        aboutButton.addActionListener(this);
+        aboutButton.setFocusable(false);
+        aboutPanel.add(aboutButton);
+
+        // setting button setup
+        settingsButton.addActionListener(this);
+        settingsButton.setFocusable(false);
+        settingsPanel.add(settingsButton);
+
+        //adding elements to the frame
+        // this.add(header); 이거 제목 쓸때 주석 빼셈
+        this.add(aboutPanel);
+        this.add(buttonsPanel);
+        this.add(settingsPanel);
+        this.add(background_image);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        //open 1p game window if pressed
+        if(e.getSource() == singlePlayerButton){
+            Tetris game = new Tetris();
+            this.setVisible(false);
+            game.setLocationRelativeTo(null);
+            game.setVisible(true);
+        }
+
+        //open about window if aboutButton is pressed
+        if(e.getSource() == aboutButton){
+            aboutFrame = new AboutFrame();
+            this.setVisible(false);
+            aboutFrame.setLocationRelativeTo(null);
+            aboutFrame.setVisible(true);
+        }
+    }
+
+
+}
